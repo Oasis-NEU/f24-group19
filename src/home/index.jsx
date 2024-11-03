10aaa188613dd9a2c57367795af0985bc3a1045c
seanyoo0405 <148388939+seanyoo0405@users.noreply.github.com>
2024-11-03 15:16:24 -0500

    local image implementation merge with database set

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -7,13 +7,15 @@ export default function home() {
     </header>
                 <div id="wd-images">
                 Select the item that you prefer:
+                <br />
                 Loading a local image:
                 <br />
-                <img id="wd-pants1" src="images/pants1.jpg" height="200px" />
-                <img id="wd-pants1" src="images/pants1.jpg" height="200px" />
-                <img id="wd-pants1" src="images/pants1.jpg" height="200px" />
-                <img id="wd-pants1" src="images/pants1.jpg" height="200px" />
-                <img id="wd-pants1" src="images/pants1.jpg" height="200px" />
+                <img id="wd-pants1" src="clothing v.2/pants/pants 1.jpg" height="200px" />
+                <img id="wd-pants2" src="clothing v.2/pants/pants 2.jpg" height="200px" />
+                <img id="wd-pants3" src="clothing v.2/pants/pants 3.jpg" height="200px" />
+                <img id="wd-pants4" src="clothing v.2/pants/pants 4.jpg" height="200px" />
+                <img id="wd-pants5" src="clothing v.2/pants/pants 5.jpg" height="200px" />
+
             </div></div>
   );}
   
